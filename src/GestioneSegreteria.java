3c35ba3502873e2d682f3f441a4db09c3da33254
Francesco <francescograuso@live.it>
2022-07-29 17:19:54 +0200

    backup

--- src/GestioneSegreteria.java
+++ src/GestioneSegreteria.java
@@ -1,13 +1,9 @@
-import org.json.simple.*;
+import java.io.*;
 
-import java.io.FileWriter;
 import java.io.IOException;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.logging.Level;
+import java.util.List;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -18,7 +14,7 @@ public class GestioneSegreteria {
     Studente s;
 
     //function that register student in json
-    public int registerUser(Studente s){
+    public int registerUser(Studente s) {
         JSONObject js = new JSONObject();
         js.put("Id", "1");
         js.put("Nome", s.getName());
@@ -26,13 +22,12 @@ public class GestioneSegreteria {
         js.put("Username", s.getUsername());
         js.put("Password", s.getPassword());
         js.put("Email", s.getEmail());
-        js.put("CF",  s.getCF());
+        js.put("CF", s.getCF());
 
         //write on JSON file
-        try (FileWriter file = new FileWriter("Studenti.json")) {
+        try (FileWriter file = new FileWriter("Studenti.json", true)) {
             file.write(js.toJSONString());
             file.flush();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -41,28 +36,34 @@ public class GestioneSegreteria {
     }
 
     //function that login student in json
-    public int loginUser(Studente s){
+    public void loginUser(Studente s) throws IOException, ParseException {
+
         JSONParser parser = new JSONParser();
-        try {
-            Object obj = parser.parse(new FileReader("Studenti.json"));
 
-            // A JSON object. Key value pairs are unordered. JSONObject supports java.util.Map interface.
-            JSONObject jsonObject = (JSONObject) obj;
+        try (Reader reader = new FileReader("Studenti.json")) {
 
-            // A JSON array. JSONObject supports java.util.List interface.
-            JSONArray companyList = (JSONArray) jsonObject.get("");
+            JSONObject jsonObject = (JSONObject) parser.parse(reader);
+            System.out.println(jsonObject);
 
-            // An iterator over a collection. Iterator takes the place of Enumeration in the Java Collections Framework.
-            // Iterators differ from enumerations in two ways:
-            // 1. Iterators allow the caller to remove elements from the underlying collection during the iteration with well-defined semantics.
-            // 2. Method names have been improved.
-            Iterator<JSONObject> iterator = companyList.iterator();
+            String name = (String) jsonObject.get("Nome");
+            System.out.println(name);
+
+            String age = (String) jsonObject.get("Cognome");
+            System.out.println(age);
+
+            // loop array
+            /*JSONArray msg = (JSONArray) jsonObject.get("messages");
+            Iterator<String> iterator = msg.iterator();
             while (iterator.hasNext()) {
                 System.out.println(iterator.next());
-            }
-        } catch (Exception e) {
+            }*/
+
+        } catch (IOException e) {
             e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+
+
         }
-        return 1;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
         Studente s = new Studente();
         s.setName("Francesco");
         s.setSurname("Grauso");
@@ -8,8 +13,17 @@ public class Main {
         s.setEmail("francescograuso@live.it");
         s.setCF("GRSFNC02M27M28B");
 
+        Studente s2 = new Studente();
+        s2.setName("Vincenzo");
+        s2.setSurname("Angelini");
+        s2.setUsername("schifName");
+        s2.setPassword("solotu");
+        s2.setEmail("vincenzoangelini@live.it");
+        s2.setCF("VNCSDIDJ23NJSDS");
+
         GestioneSegreteria gs = new GestioneSegreteria();
-        gs.registerUser(s);
-        gs.loginUser(s);
+      // gs.registerUser(s);
+      // gs.registerUser(s2);
+       gs.loginUser(s);
     }
 }
