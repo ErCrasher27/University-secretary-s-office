9d902e14872f4d22c32242fdf256e4afbab1929c
Francesco <francescograuso@live.it>
2022-07-29 13:15:37 +0200

    backend per fra

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,18 @@
-//import java swing and awt needed for UI
-import javax.swing.*;
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
+        Studente s1 = new Studente();
+        s1.setName("Francesco");
+        s1.setSurname("Grauso");
+        s1.setUsername("ErCrasher");
+        s1.setEmail("francescograuso@live.it");
+        s1.setCF("GRSFNC02M27M28B");
 
-        //create frame obj and set poperties
-        JFrame frame = new JFrame();
-        frame.setTitle("Login Form");
-        frame.setVisible(true);
-        frame.setLayout(null);
-        frame.setSize(400,500);//400 width and 500 height
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(true);
+        System.out.println(s1.getName());
+        System.out.println(s1.getSurname());
+        System.out.println(s1.getUsername());
+        System.out.println(s1.getEmail());
+        System.out.println(s1.getCF());
 
-        //create label login
-        JLabel l_login = new JLabel("Login");
-        //create labels user and pw
-        JLabel l_user=new JLabel("USERNAME");
-        JLabel l_password=new JLabel("PASSWORD");
-
-        //create textfield user and pw
-        JTextField field_user=new JTextField();
-        JPasswordField field_pw=new JPasswordField();
-
-        //create button login
-        JButton b_login=new JButton("LOGIN");
-
-        //properties of all this components
-        l_user.setBounds(50,150,100,30);
-        l_password.setBounds(50,220,100,30);
-        field_user.setBounds(150,150,150,30);
-        field_pw.setBounds(150,220,150,30);
-        b_login.setBounds(125,290,100, 40);
-
-        //add element in frame
-        frame.add(l_user);
-        frame.add(field_user);
-        frame.add(l_password);
-        frame.add(field_pw);
-        frame.add(b_login);
 
     }
 }
---
+++ src/Studente.java
@@ -0,0 +1,67 @@
+/*Class Students containing all functions for operations on student data*/
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+//import org.json.*;
+/*Creation of the class Students*/
+public class Studente {
+    private String name, surname, username, email, cf = null;
+
+    /*Creation of the procedure for the acquisition of the student’s name*/
+    public void setName(String name){
+        this.name = name;
+    }
+    /*Creation of function for visualization of the student’s name*/
+    public String getName(){
+        return this.name;
+    }
+
+    /*Creation of the procedure for the acquisition of the student’s surname*/
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    /*Creation of function for visualization of the student’s surname*/
+    public String getSurname(){
+        return this.surname;
+    }
+
+    /*Creation of the procedure for the acquisition of the student’s username*/
+    public void setUsername(String username){
+        this.username = username;
+    }
+    /*Creation of function for visualization of the student’s username*/
+    public String getUsername(){
+        return this.username;
+    }
+
+    /*Creation of the procedure for the acquisition of the student’s email*/
+    public void setEmail(String email){
+        /*Checking for the @ in the email address. If it is present, its existence is verified*/
+        Pattern pattern = Pattern.compile("@", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        boolean matchFound = matcher.find();
+        if(matchFound) {
+            this.email = email;
+        } else {
+            System.out.println("email not valid");
+        }
+    }
+    /*Creation of function for visualization of the student’s email*/
+    public String getEmail(){
+        return this.email;
+    }
+
+    /*Creation of the procedure for the acquisition of the student’s CF*/
+    public void setCF(String cf){
+        this.cf = cf;
+    }
+    /*Creation of function for visualization of the student’s CF*/
+    public String getCF(){
+        return this.cf;
+    }
+}
