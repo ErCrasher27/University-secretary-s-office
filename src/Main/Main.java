10aa7bec1a710abe9e18a6ddf56aa88417b127ad
Apocalixs-Nick <nicolapiccirillo04@gmail.com>
2022-08-10 16:42:44 +0200

    Backup whit Gradle
    but icons doesn't works

--- src/Main/Main.java
+++
@@ -1,8 +0,0 @@
-package Main;
-import Forms.Login.FormLogin;
-public class Main {
-    public static void main(String[] args) {
-        FormLogin fl = new FormLogin();
-        fl.init();
-    }
-}
---
+++ src/Main/java/UniversitySecretary/Main.java
@@ -0,0 +1,9 @@
+package UniversitySecretary;
+
+import Forms.Login.FormLogin;
+public class Main {
+    public static void main(String[] args) {
+        FormLogin fl = new FormLogin();
+        fl.init();
+    }
+}
